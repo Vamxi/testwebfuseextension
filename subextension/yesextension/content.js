1deb894008ca7342acbdd29c260e6f8bef4875a4
Vamxi <vamsi@surfly.com>
2025-12-02 15:51:10 +0100

    Latest development on main

--- subextension/yesextension/content.js
+++ subextension/yesextension/content.js
@@ -1,2 +1,2 @@
-console.log("Version: v2.0.0 TAG");
-  console.log("Major version 2.0.0 - breaking changes!");
+console.log("Version: MAIN BRANCH - LATEST DEVELOPMENT");
+  console.log("This is the bleeding edge main branch!");
