b8fa3ffe0b6053937dfaf16590277f7ec3f86075
Vamxi <vamsi@surfly.com>
2025-12-02 15:47:58 +0100

    Feature branch version

--- subextension/yesextension/content.js
+++ subextension/yesextension/content.js
@@ -1,2 +1,2 @@
-console.log("Version: v2.0.0 TAG");
-  console.log("Major version 2.0.0 - breaking changes!");
+console.log("Version: FEATURE BRANCH");
+  console.log("This is from the feature/testing branch");
